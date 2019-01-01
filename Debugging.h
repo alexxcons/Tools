2ce1be2587746009a9849628a6de0356ddd684c1
Alexander Schwinn <alexxcons@xfce.org>
2019-01-01 21:13:26 +0100

    works now with timestamp

--- Debugging.h
+++ Debugging.h
@@ -1,12 +1,24 @@
 
 
 
+#include <sys/time.h>
+
+static
+long long get_timestamp_milli() {
+    struct timeval te;
+    gettimeofday(&te, NULL); // get current time
+    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // calculate milliseconds
+    // printf("milliseconds: %lld\n", milliseconds);
+    return milliseconds;
+}
+
+
 static void
-debug_to_file (const char *message, gboolean append = TRUE)
+debug_to_file (const char *message)
 {
   FILE *f;
   // file will be created, if not existing
-if(append)
+if(TRUE)
   f = fopen("/home/schwinn/thunarDebug.txt", "a");
 else
   f = fopen("/home/schwinn/thunarDebug.txt", "w"); // this will overwrite an existing file
@@ -14,6 +26,11 @@ if (f == NULL)
 {
     printf("Error opening file!\n");
     exit(1);
+}
+
+/* print some text */
+fprintf(f, "[%lld]: %s\n", get_timestamp_milli(), message);
+fclose(f);
 }
 
 // copy + paste
